cfa04bb819a029cd4d22f1b82ecc8cfc08f5ccf6
Connor Knights <100325645+Lagswastaken@users.noreply.github.com>
2023-03-05 20:45:20 +0000

    Started Coding!

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random, discord
 #RANDOM allows the bot to generate random proceedings
 from discord.ext import commands
-token = ""
-
-
+token = "ODg3ODEyMjMwMTg2NDI2NDEw.G-g9kI.EMXYjdc3sZTOHHYb1m0QNYo20AnWNZxHldv5JE"
+client = commands.Bot(command_prefix )
+client.run
