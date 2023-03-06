77c3d165bdf24a02cd5ece193b8483342433b8df
Connor Knights <100325645+Lagswastaken@users.noreply.github.com>
2023-03-06 21:20:42 +0000

    Hidden my bot token

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random, discord
+import random, discord,os
 #RANDOM allows the bot to generate random proceedings
 from discord.ext import commands
-token = ""
-
-
+TOKEN = os.getenv["token"]
+client = commands.Bot(command_prefix )
+client.run(TOKEN)
