f376f2039ae31de7154d8c4785d183a9844f5f69
Connor Knights <100325645+Lagswastaken@users.noreply.github.com>
2023-03-14 21:58:02 +0000

    Started coding the PSR game

--- main.py
+++ main.py
@@ -21,6 +21,44 @@ async def roll(ctx,num):
     d = random.randint(1,6)
     await ctx.channel.send(f"Rolling a D6: {d}")
 
+#Paper Scissors Rock
+@bot.command(name='psr')
+async def psr(ctx):
+  computer_choice = ''
+  await ctx.channel.send("Lets play Paper, Scissors, Rock!")
+  choices = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(choices)
+  player_choice = ''
+  userPlaying = True
+  await ctx.channel.send("What do you choose?")
+  if userPlaying:
+    response = await bot.wait_for("message")
+  if response.content == 'paper':
+     player_choice = 'paper'
+    
+  elif response.content == 'rock':
+     player_choice = 'rock'
+
+  elif response.content == 'scissors':
+     player_choice = 'scissors'
+
+  if player_choice == computer_choice:
+        result = "It's a tie!"
+  elif player_choice == "rock" and computer_choice == "scissors":
+        result = "You win!"
+  elif player_choice == "paper" and computer_choice == "rock":
+        result = "You win!"
+  elif player_choice == "scissors" and computer_choice == "paper":
+        result = "You win!"
+  else:
+        result = "Computer wins."
+
+        await ctx.channel.send(f"You chose {player_choice}, and the computer chose {computer_choice}. {result}")
+
+  
+   
+  
+
 #Blackjack Game Code
 
 
@@ -62,17 +100,20 @@ def showDealerHand():
     return dealerHand[0], dealerHand[1]
 
 #Making the game run
-for _ in range(2):
-  dealCard(dealerHand)
-  dealCard(playerHand)
 
-#A loop that breaks once 
+
+
 @client.event
 async def on_ready():
   print('Logged in as {0.user}'.format(client))
 
 @bot.command(name = 'blackjack')
 async def blackjack(ctx):
+        playerHand.clear()
+        dealerHand.clear()
+        for _ in range(2):
+          dealCard(dealerHand)
+          dealCard(playerHand)
         playerIn = True
         dealerIn = True
         await ctx.channel.send("Welcome to blackjack!")
@@ -80,7 +121,7 @@ async def blackjack(ctx):
            await ctx.channel.send(f"Dealer has {showDealerHand()} and X")
            await ctx.channel.send(f"You have {playerHand} for a total of {total(playerHand)}")
            if playerIn:
-                response =await bot.wait_for("message")
+                response = await bot.wait_for("message")
            if response.content == 'stand':
                     playerIn = False
            elif response.content == 'hit':
@@ -117,11 +158,11 @@ async def blackjack(ctx):
             await ctx.channel.send(f"\n You have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}")
             await ctx.channel.send("You both stood! The dealer was closer to 21! Dealer wins!")
             break
-           elif 21 - total(dealerhand) > 21 - total(playerHand):
+           elif 21 - total(dealerHand) > 21 - total(playerHand):
             await ctx.channel.send(f"\n You have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}")
             await ctx.channel.send("You both stood! You were closer to 21! You win!")
             break
-           elif 21 - total(dealerhand) == 21 - total(playerHand):
+           elif 21 - total(dealerHand) == 21 - total(playerHand):
             await ctx.channel.send(f"\n You have {playerHand} for a total of {total(playerHand)} and the dealer has {dealerHand} for a total of {total(dealerHand)}")
             await ctx.channel.send("You Draw! No one wins!")
             break
